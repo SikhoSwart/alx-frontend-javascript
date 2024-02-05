747ec25b4f0fa79de97f91713540ad7f1a8367fd
SikhoSwart <sikhoswart@gmail.com>
2024-02-05 17:00:25 +0000

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    //Dealing with eslint
     const task = true;
-    const task2 = false;
+    //Dealing with eslint
+    const task2 = false; 
   }
 
   return [task, task2];
