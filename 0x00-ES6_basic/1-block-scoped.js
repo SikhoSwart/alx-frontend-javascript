85d5064c6a1c5cf43b4c2559dc39ac1ca5d3095e
SikhoSwart <sikhoswart@gmail.com>
2024-02-05 16:57:10 +0000

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line
-    let task = true;
-    // eslint-disable-next-line
-    let task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
