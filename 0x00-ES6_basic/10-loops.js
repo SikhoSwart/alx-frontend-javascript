a7be2df6fe6ca6f134b8346e83575381857a8130
Sikho <129541577+SikhoSwart@users.noreply.github.com>
2024-02-06 00:32:22 +0200

    Task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr = [];
-  for (const idx of array) {
+  for (let idx of array) {
     const value = idx;
     arr.push(appendString + value);
   }
