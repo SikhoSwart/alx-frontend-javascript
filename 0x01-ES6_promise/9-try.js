c7212abab0033baef4b5f877c3ed2c8067cbac3a
SikhoSwart <sikhoswart@gmail.com>
2024-02-07 14:40:21 +0000

    Task 8 and 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const arr = [];
+
+  try {
+    arr.push(mathFunction());
+  } catch (err) {
+    arr.push(String(err));
+  } finally {
+    arr.push('Guardrail was processed');
+  }
+
+  return arr;
+}
