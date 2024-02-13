e2d167aa4acfadde93fd338793842c4f58e98b59
SikhoSwart <sikhoswart@gmail.com>
2024-02-13 07:20:03 -0800

    Task 4 and 5

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return arr;
+  return arr
     .filter(student => student.location === city)
     .map(student => {
       let grade = newGrades.find(g => g.studentId === student.id);
